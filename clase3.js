6f941d5c4d2e97f908a2a10f740adc5da121bed4
Luis Guzmán <luisguzman1988@gmil.com>
2021-07-09 17:03:14 -0400

    agregando otras clases

--- clase3.js
+++ clase3.js
@@ -13,7 +13,27 @@ edad = edad + 1
 
 edad1 += 1
 
+var precioVino = 200.3
 
+// var total  = precioVino * 3
+
+var total = precioVino * 3
+
+// las operaciones con decimales no son precisa, por lo que se debe hacer lo siguiente
+
+
+var total1 = precioVino * 100 * 3 / 100
+
+// otra forma un poco mas precisa
+var total2 = Math.round(precioVino * 100 * 3) / 100
+
+// si quieres elegir la cantidad de decimales que quieres
+
+var total3 = total.toFixed(3)
+
+// parse float el float se le llama a los numeros decimales
+
+var total4 = parseFloat(total3)
 
 // OPERADORES
 
