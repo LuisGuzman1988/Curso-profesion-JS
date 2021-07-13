d627d3dca0f6bffac5dd4e5b68b294d6bc3e338d
Luis Guzmán <luisguzman1988@gmail.com>
2021-07-12 20:13:05 -0400

    Trabajando con objetos

--- clase6.js
+++ clase6.js
@@ -1,5 +1,5 @@
 // OBJETOS, DEFINIR ATRIBUTOS Y ASIGNAR VALORES
-
+// clave y valor 
 
 var luis = {
     nombre: 'Luis',
@@ -7,11 +7,32 @@ var luis = {
     edad: 33
 }
 
+var jari = {
+    nombre: "Jaribel",
+    apellido: "Nova",
+    edad: 28
+}
+
 function imprimirNombreEnMayuscula(nombre) { 
     nombre = nombre.toUpperCase()
     console.log(nombre)
 }
 
+// mejorando la funcion 
+
+function imprimirNombreEnMayuscula2(persona) {
+    var nombre = persona.nombre.toUpperCase()
+    console.log(nombre)
+}
+
+// otra forma por si no quieres crear una nueva variable
+
+function imprimirNombreEnMayuscula3(persona) {
+    console.log(persona.nombre.toUpperCase())
+}
+
 // para acceder a las claves del objeto se usa .nombre o .apellido o .edad, depende que valor que quieras
 
 imprimirNombreEnMayuscula(luis.nombre)
+imprimirNombreEnMayuscula2(jari)
+imprimirNombreEnMayuscula3(luis)
